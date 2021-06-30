57a17960a4800a510c48fc1d7a48c336c59f49fc
eneserbap <31405022+eneserbap@users.noreply.github.com>
2021-06-30 12:45:41 +0300

    Update wp.py

--- wp.py
+++ wp.py
@@ -1,7 +1,7 @@
 #pip install pywhatkit
-import pywhatkit as kit
+import pywhatkit as pyw
 
-kit.sendwhatmsg("recievernumber","message",hour,minute)
+pyw.sendwhatmsg("recievernumber","message",hour,minute)
 
 #example:
-# kit.sendwhatmsg("+90123******","hello",12,45)
+# pyw.sendwhatmsg("+90123******","hello",12,45)
